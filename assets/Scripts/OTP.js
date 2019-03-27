dddc159cb47602e38f35e086735b24240c99e231
SathishSaminathan <Sathishcse1975@gmail.com>
2019-03-27 15:30:36 +0530

    bug_fixed

--- assets/Scripts/OTP.js
+++ assets/Scripts/OTP.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
   var charLimit = 1;
+  $("#input_1").focus();
   $(".otp_container").click(() => {
-    $("#input_1").focus();
+    // $("#input_1").focus();
   });
   $(".inputs")
     .keydown(function(e) {
@@ -23,36 +24,37 @@ $(document).ready(function() {
         46,
         144,
         145,
-        105,
-        97,
-        98,
-        99,
-        100,
-        101,
-        102,
-        103,
-        104
+        105
       ];
-      if (e.which == 8 && this.value.length == 0) {
-        $(this)
-          .prev(".inputs")
-          .focus();
-      } else if ($.inArray(e.which, keys) >= 0) {
-        return true;
-      } else if (this.value.length >= charLimit) {
-        $(this)
-          .next(".inputs")
-          .focus();
-        return false;
-      } else if (e.shiftKey || e.which <= 48 || e.which >= 58) {
+      if (this.value.length < charLimit || e.which == 8) {
+        if (e.which == 8 && this.value.length == 0) {
+          console.log("1");
+          $(this)
+            .prev(".inputs")
+            .focus();
+        } else if (
+          $.inArray(e.which, keys) >= 0 ||
+          (e.which >= 97 && e.which <= 104)
+        ) {
+          return true;
+        } else if (this.value.length >= charLimit) {
+          $(this)
+            .next(".inputs")
+            .focus();
+          return false;
+        } else if (e.shiftKey || e.which <= 48 || e.which >= 58) {
+          return false;
+        }
+      } else {
         return false;
       }
     })
     .keyup(function() {
+      // console.log('keyup')
       if (this.value.length >= charLimit) {
         if ($(this).is(":last-child")) {
-          $(".inputs").val("");
-          $("#input_1").focus();
+          // $(".inputs").val("");
+          // $("#input_1").focus();
         } else {
           $(this)
             .next(".inputs")
